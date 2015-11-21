58ed1ca8a8ad589e2d26ca5e3fbe208fdbe47d77
Eben <me@ebenroux.co.za>
2015-11-21 12:02:43 +0200

    - updated packages

--- Shuttle.ESB.Modules.Tests/MessageForwardingSectionTests.cs
+++ Shuttle.ESB.Modules.Tests/MessageForwardingSectionTests.cs
@@ -1,23 +1,24 @@
 using System;
+using System.IO;
 using NUnit.Framework;
 using Shuttle.ESB.Core;
 
 namespace Shuttle.ESB.Modules.Tests
 {
     [TestFixture]
-	public class MessageForwardingSectionTests
+    public class MessageForwardingSectionTests
     {
         [Test]
-		[TestCase("MessageForwarding.config")]
-		[TestCase("MessageForwarding-Grouped.config")]
+        [TestCase("MessageForwarding.config")]
+        [TestCase("MessageForwarding-Grouped.config")]
         public void Should_be_able_to_load_the_configuration(string file)
         {
-			var section = ShuttleConfigurationSection.Open<MessageForwardingSection>("messageForwarding", string.Format(@"config-files\{0}", file));
+            var section = ShuttleConfigurationSection.Open<MessageForwardingSection>("messageForwarding", Path.Combine(AppDomain.CurrentDomain.BaseDirectory, string.Format(@"config-files\{0}", file)));
 
             Assert.IsNotNull(section);
             Assert.AreEqual(2, section.ForwardingRoutes.Count);
 
-			foreach (MessageRouteElement map in section.ForwardingRoutes)
+            foreach (MessageRouteElement map in section.ForwardingRoutes)
             {
                 Console.WriteLine(map.Uri);
 
@@ -25,7 +26,7 @@ namespace Shuttle.ESB.Modules.Tests
                 {
                     Console.WriteLine("-> {0} - {1}", specification.Name, specification.Value);
                 }
-                
+
                 Console.WriteLine();
             }
         }
--- Shuttle.ESB.Modules.Tests/PurgeQueuesSectionTests.cs
+++ Shuttle.ESB.Modules.Tests/PurgeQueuesSectionTests.cs
@@ -1,23 +1,24 @@
 using System;
+using System.IO;
 using NUnit.Framework;
 using Shuttle.ESB.Core;
 
 namespace Shuttle.ESB.Modules.Tests
 {
     [TestFixture]
-	public class PurgeQueuesSectionTests
+    public class PurgeQueuesSectionTests
     {
         [Test]
-		[TestCase("PurgeQueues.config")]
-		[TestCase("PurgeQueues-Grouped.config")]
+        [TestCase("PurgeQueues.config")]
+        [TestCase("PurgeQueues-Grouped.config")]
         public void Should_be_able_to_load_the_configuration(string file)
         {
-			var section = ShuttleConfigurationSection.Open<PurgeQueuesSection>("purgeQueues", string.Format(@"config-files\{0}", file));
+            var section = ShuttleConfigurationSection.Open<PurgeQueuesSection>("purgeQueues", Path.Combine(AppDomain.CurrentDomain.BaseDirectory, string.Format(@"config-files\{0}", file)));
 
             Assert.IsNotNull(section);
             Assert.AreEqual(2, section.Queues.Count);
 
-			foreach (PurgeQueueElement element in section.Queues)
+            foreach (PurgeQueueElement element in section.Queues)
             {
                 Console.WriteLine(element.Uri);
             }
